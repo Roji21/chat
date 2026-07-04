17399fb60cb8072b427733b56ea8b3df022db076
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2026-07-04 23:37:49 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,16 +5,25 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Cek apakah aplikasi sedang dalam mode pemeliharaan (maintenance)...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Registrasi Composer Autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// Memuat Bootstrap Laravel dan menjalankan aplikasi...
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+if ($app instanceof Application) {
+    $app->handleRequest(Request::capture());
+} else {
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
+
+    $kernel->terminate($request, $response);
+}
