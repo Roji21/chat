78bfb4a422df8bb4e6b20502e7a74ca163f528a4
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2026-07-04 23:20:47 +0700

    2

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -13,44 +13,26 @@ class SettingController extends Controller
     // Menampilkan halaman utama
     public function index(Request $request)
     {
-        $user = session('user_id'); 
-        $iduser = DB::table('user')->where('id_user', $user)->first();
-        $id = $iduser->id_user;
-        $userSekarang = $iduser->nama;
-        $riwayatPesan = DB::table('pesan')->where('pengirim',$user)->get();
-        $lawanBicara = '';
-        $foto= $iduser->foto ;
-        $fotolawan = 'default.jpeg';
-        return view('setting', compact('id','userSekarang', 'lawanBicara','riwayatPesan', 'foto', 'fotolawan'));
+        return view('setting');
     }
     public function upfoto(Request $request)
     {
     $request->validate([
         'foto_profil' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Maksimal 2MB
     ]);
-    $userId = session('user_id');
-    $nama = db::table('user')->where('id_user', $userId)->first();
+    $userId = auth()->user()->number;
+    $nama = db::table('custom_users')->where('number', $userId)->first();
     if ($request->hasFile('foto_profil')) {
         $file = $request->file('foto_profil');
 
-        $namaFile = 'user_' . $nama->nama . '_' . time() . '.' . $file->getClientOriginalExtension();
+        $namaFile = 'user_' . $nama->name . '_' . time() . '.' . $file->getClientOriginalExtension();
         
         $tujuanFolder = public_path('storage/img');
-        // if ($nama && $nama->foto) {
-        //         // Gabungkan jalur folder dengan nama file lama yang ada di database
-        //         $jalurFotoLama = $tujuanFolder . '/' . $nama->foto;
-
-        //         // Cek apakah file lama tersebut benar-benar ada secara fisik di folder public
-        //         if (File::exists($jalurFotoLama)) {
-        //             File::delete($jalurFotoLama); // Hapus file lama dari penyimpanan
-        //         }
-        // }
+        
         $file->move($tujuanFolder, $namaFile);
-
-        $userId = session('user_id');
-        DB::table('user')
-            ->where('id_user', $userId)
-            ->update(['foto' => $namaFile]);
+        DB::table('custom_users')
+            ->where('number', $userId)
+            ->update(['photo' => $namaFile]);
         
             return response()->json([
                 'success' => true,
@@ -71,16 +53,16 @@ class SettingController extends Controller
             'nilai_teks' => 'required|string|max:255',
         ]);
 
-        $userId = session('user_id');
+        $userId = auth()->user()->number;
         if (!$userId) {
             return response()->json(['success' => false, 'message' => 'User tidak terautentikasi.'], 401);
         }
 
         // 3. Tentukan kolom database berdasarkan ID input dari JavaScript
         if ($request->tipe_input === 'input-name') {
-            db::table('user')->where('id_user', $userId)->update(['nama' => $request->nilai_teks]);    
+            db::table('custom_users')->where('number', $userId)->update(['name' => $request->nilai_teks]);    
         } elseif ($request->tipe_input === 'input-about') {
-            db::table('user')->where('id_user', $userId)->update(['about' => $request->nilai_teks]);
+            db::table('custom_user')->where('number', $userId)->update(['about' => $request->nilai_teks]);
         } else {
             return response()->json(['success' => false, 'message' => 'Tipe input tidak valid.'], 400);    
         }
@@ -92,13 +74,7 @@ class SettingController extends Controller
     }
     public function profile(Request $request)
     {
-        $user = session('user_id'); 
-        $iduser = DB::table('user')->where('id_user', $user)->first();
-        $id = $iduser->id_user;
-        $userSekarang = $iduser->nama;
-        $foto= $iduser->foto ;
-        $about = $iduser->about;
-        return view('profile', compact('id','userSekarang','foto','about'));
+        return view('profile');
     }
     public function notification(Request $request)
     {
