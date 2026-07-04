e73c222729cf23652ecfcfc52ca544b3d5516aa4
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2026-07-04 20:57:21 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Mengarahkan pengguna yang belum login ke rute '/' (halaman utama Anda)
+        $middleware->redirectGuestsTo('/');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
