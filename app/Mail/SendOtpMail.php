f99a40ead70e50f50ff769066b1ae49b58272da7
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2026-07-05 08:37:28 +0700

    Update SendOtpMail.php

--- app/Mail/SendOtpMail.php
+++ app/Mail/SendOtpMail.php
@@ -39,7 +39,7 @@ class SendOtpMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.otp', // <- Pastikan di sini tertulis 'emails.otp'
+            text: 'emails.otp', // <- Pastikan di sini tertulis 'emails.otp'
         );
     }
 
